f35d501c98de314bc0aa78e57857dace5c88f2c5
YOUSSOUF ISMAIL CHERIFI <youcef.ismail.cherifi@gmail.com>
2019-08-16 19:39:33 +0100

    Add date and time labels

---
+++ Assets/CctvDataLabelScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CctvDataLabelScript : MonoBehaviour
+{
+    Text dateLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dateLabel = GetComponent<Text>();
+        dateLabel.text = System.DateTime.Now.ToString("dd/MM/yyyy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CctvTimeLabelScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CctvTimeLabelScript : MonoBehaviour
+{
+    Text timeLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLabel = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLabel.text = System.DateTime.Now.ToString("hh:mm:ss");
+    }
+}
